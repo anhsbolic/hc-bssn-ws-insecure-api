05f970f249d88a6d50ab3c3d8a09136986c697db
Anhar Solehudin <anhsbolic@gmail.com>
2025-07-30 16:31:01 +0700

    login api

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,20 +4,17 @@ const pool = require('../db');
 const SECRET = "1234"; // Hardcoded secret
 
 exports.login = async (req, res) => {
-    const {username, password} = req.body;
+    const {email, password} = req.body;
 
     try {
         const user = await pool.query(`SELECT *
                                        FROM users
-                                       WHERE username = '${username}'
+                                       WHERE username = '${email}'
                                          AND password = '${password}'`);
         // Insecure: SQL injection possible
 
         if (user.rowCount === 0) return res.status(401).send("Invalid credentials");
-        const token = jwt.sign({
-            id: user.rows[0].id,
-            role: 'user'
-        }, SECRET); // No expiry
+        const token = jwt.sign(user.rows[0], SECRET); // No expiry
         res.json({token});
     } catch (err) {
         res.status(500).send(err);
